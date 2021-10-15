d436816ab1e0bad9277378ec078ce2cf4d0bca00
Henry Lee <henry.lee@tabcorp.com.au>
2021-10-15 17:27:32 +1100

    move inline css

--- src/SearchResultPage/Card/HotelInfoSection/index.tsx
+++ src/SearchResultPage/Card/HotelInfoSection/index.tsx
@@ -1,15 +1,15 @@
 import * as React from "react";
 
-import RatingSymbol from './RatingSymbol';
+import RatingSymbol from "./RatingSymbol";
 import "./index.scss";
 
 type Props = {
   hotelName: string;
-  address: string,
-  offerName: string,
-  isCancelFeeFree: boolean,
-  ratingType: string,
-  ratingValue: number,
+  address: string;
+  offerName: string;
+  isCancelFeeFree: boolean;
+  ratingType: string;
+  ratingValue: number;
 };
 
 const HotelInfoSection: React.FC<Props> = ({
@@ -19,40 +19,21 @@ const HotelInfoSection: React.FC<Props> = ({
   isCancelFeeFree,
   ratingType,
   ratingValue,
- }) => (
+}) => (
   <div className="hotel-info-section">
     <div>
-      <h2
-        style={{
-          fontSize: "18px",
-          fontWeight: 600,
-          margin: "0",
-          maxWidth: "310px",
-          whiteSpace: "nowrap",
-          overflow: "hidden",
-          textOverflow: "ellipsis",
-          display: "inline-block",
-        }}
-        data-testid="hotel-name"
-      >
+      <h2 className="hotel-name" data-testid="hotel-name">
         {hotelName}
       </h2>
-      <div
-        style={{
-          display: "inline-block",
-        }}
-      >
-        {
-          [1,2,3,4,5].map(number =>  (
-            <RatingSymbol
-              key={number}
-              type={ratingType}
-              ratingValue={ratingValue}
-              currentNumber={number}
-            />
-          ))
-        }
-
+      <div className="rating-wrapper">
+        {[1, 2, 3, 4, 5].map((number) => (
+          <RatingSymbol
+            key={number}
+            type={ratingType}
+            ratingValue={ratingValue}
+            currentNumber={number}
+          />
+        ))}
       </div>
     </div>
 
